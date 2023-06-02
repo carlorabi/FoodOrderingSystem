9fdc93640df14490929293f211ae15d056d69691
John Carlo Rabi <21-09945@g.batstate-u.edu.ph>
2023-06-02 10:55:55 +0800

    initial commit

--- order.py
+++ order.py
@@ -1,22 +1,21 @@
-# order.py
-from menu_item import MenuItem
+from order_item import OrderItem
 
 class Order:
     def __init__(self):
         self.items = []
 
-    def add_item(self, item):
-        self.items.append(item)
-
-    def remove_item(self, item_name):
-        for item in self.items:
-            if item.name.lower() == item_name.lower():
-                self.items.remove(item)
-                return True
-        return False
+    def add_item(self, menu_item, quantity):
+        order_item = OrderItem(menu_item, quantity)
+        self.items.append(order_item)
 
     def calculate_total_cost(self):
         total_cost = 0
-        for item in self.items:
-            total_cost += item.price
+        for order_item in self.items:
+            total_cost += order_item.menu_item.price * order_item.quantity
         return total_cost
+
+    def __str__(self):
+        order_str = ""
+        for index, order_item in enumerate(self.items):
+            order_str += f"{index+1}. {order_item.menu_item.name}: ₱{order_item.menu_item.price:.2f} x {order_item.quantity}\n"
+        return order_str.strip()
